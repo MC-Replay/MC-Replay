9b357727f43398342db67127d459adbc947da21d
Itz_KiwiSap_ <programmingjesse@gmail.com>
2022-08-22 16:06:55 +0200

    Removed RecordingSessionHandler

--- API/src/main/java/mc/replay/api/recording/IRecordingHandler.java
+++ API/src/main/java/mc/replay/api/recording/IRecordingHandler.java
@@ -3,11 +3,17 @@ package mc.replay.api.recording;
 import mc.replay.api.recording.contestant.RecordingContestant;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+import java.util.UUID;
+
 public interface IRecordingHandler {
 
-    @NotNull
-    IRecordingSessionHandler getRecordingSessionHandler();
+    Map<UUID, RecordingSession> getRecordingSessions();
 
     @NotNull
     RecordingSession startRecording(@NotNull RecordingContestant contestant);
+
+    @NotNull Recording stopRecording(@NotNull UUID sessionUuid);
+
+    boolean cancelRecording(@NotNull UUID sessionUuid);
 }
--- Recording/src/main/java/mc/replay/recording/RecordingHandler.java
+++ Recording/src/main/java/mc/replay/recording/RecordingHandler.java
@@ -2,26 +2,43 @@ package mc.replay.recording;
 
 import lombok.Getter;
 import mc.replay.api.recording.IRecordingHandler;
+import mc.replay.api.recording.Recording;
 import mc.replay.api.recording.RecordingSession;
 import mc.replay.api.recording.contestant.RecordingContestant;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 @Getter
 public final class RecordingHandler implements IRecordingHandler {
 
-    private final RecordingSessionHandler recordingSessionHandler;
-
-    public RecordingHandler() {
-        this.recordingSessionHandler = new RecordingSessionHandler();
-    }
+    private final Map<UUID, RecordingSessionImpl> recordingSessions = new HashMap<>();
 
     @Override
     public @NotNull RecordingSession startRecording(@NotNull RecordingContestant contestant) {
         RecordingSessionImpl recordingSession = new RecordingSessionImpl(contestant);
-        return this.recordingSessionHandler.addSession(recordingSession);
+        this.recordingSessions.put(recordingSession.getSessionUuid(), recordingSession);
+        return recordingSession;
+    }
+
+    @Override
+    public @NotNull Recording stopRecording(@NotNull UUID sessionUuid) {
+        RecordingSession recordingSession = this.recordingSessions.remove(sessionUuid);
+        if (recordingSession == null) {
+            throw new IllegalStateException("No recording session found for session uuid '" + sessionUuid + "'");
+        }
+
+        return new RecordingImpl(recordingSession.getStartTime(), recordingSession.getRecordables());
+    }
+
+    @Override
+    public boolean cancelRecording(@NotNull UUID sessionUuid) {
+        return this.recordingSessions.remove(sessionUuid) != null;
     }
 
     public boolean shouldRecord() {
-        return !this.recordingSessionHandler.getRecordingSessions().isEmpty();
+        return !this.recordingSessions.isEmpty();
     }
 }
