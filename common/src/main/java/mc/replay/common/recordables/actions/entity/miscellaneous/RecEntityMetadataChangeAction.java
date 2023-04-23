6fa0424874d47fd4293425ae09363a7946d54f3f
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-04-23 02:11:50 +0200

    If metadata is an instance of ShooterProvider, replace the shooter's id with the id of the replay entity

--- common/src/main/java/mc/replay/common/recordables/actions/entity/miscellaneous/RecEntityMetadataChangeAction.java
+++ common/src/main/java/mc/replay/common/recordables/actions/entity/miscellaneous/RecEntityMetadataChangeAction.java
@@ -3,12 +3,15 @@ package mc.replay.common.recordables.actions.entity.miscellaneous;
 import mc.replay.api.recording.recordables.action.EntityRecordableAction;
 import mc.replay.api.recording.recordables.entity.RecordableEntityData;
 import mc.replay.common.recordables.types.entity.miscellaneous.RecEntityMetadataChange;
+import mc.replay.packetlib.data.entity.Metadata;
 import mc.replay.packetlib.network.packet.clientbound.ClientboundPacket;
 import mc.replay.packetlib.network.packet.clientbound.play.ClientboundEntityMetadataPacket;
+import mc.replay.wrapper.entity.metadata.ShooterProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 public record RecEntityMetadataChangeAction() implements EntityRecordableAction<RecEntityMetadataChange> {
@@ -19,6 +22,18 @@ public record RecEntityMetadataChangeAction() implements EntityRecordableAction<
         if (data == null) return List.of();
 
         data.entity().addMetadata(recordable.metadata());
+
+        if (data.entity().getMetadata() instanceof ShooterProvider provider) {
+            Integer shooterId = provider.getShooterId();
+            RecordableEntityData shooterData = function.apply(shooterId == null ? -1 : shooterId);
+            if (shooterData == null) return List.of();
+
+            provider.setShooterId(shooterData.entityId());
+
+            for (Map.Entry<Integer, Metadata.Entry<?>> entry : data.entity().getMetadata().getEntries().entrySet()) {
+                recordable.metadata().replace(entry.getKey(), entry.getValue());
+            }
+        }
 
         return List.of(
                 new ClientboundEntityMetadataPacket(
