85d74aa2517d9118037150a8f20d22e40239c914
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-04-23 02:58:27 +0200

    Updated packetlib and added vehicle position recordables to the tick dispatcher

--- common/src/main/java/mc/replay/common/dispatcher/tick/EntityLocationTickDispatcher.java
+++ common/src/main/java/mc/replay/common/dispatcher/tick/EntityLocationTickDispatcher.java
@@ -10,6 +10,7 @@ import mc.replay.common.recordables.types.entity.movement.RecEntityTeleport;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Vehicle;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +19,8 @@ import java.util.Map;
 
 public final class EntityLocationTickDispatcher implements DispatcherTick {
 
-    private final Map<LivingEntity, Location> lastLocations = new HashMap<>();
-    private final Map<LivingEntity, Integer> positionAndRotationAmounts = new HashMap<>();
+    private final Map<Entity, Location> lastLocations = new HashMap<>();
+    private final Map<Entity, Integer> positionAndRotationAmounts = new HashMap<>();
 
     @Override
     public void onTickGlobal(Integer currentTick) {
@@ -29,27 +30,27 @@ public final class EntityLocationTickDispatcher implements DispatcherTick {
 
     @Override
     public List<Recordable> getRecordables(int currentTick, Entity entity) {
-        if (!(entity instanceof LivingEntity livingEntity)) return null;
+        if (!(entity instanceof LivingEntity) && !(entity instanceof Vehicle)) return null;
 
         List<Recordable> recordables = new ArrayList<>();
 
-        Location currentLocation = livingEntity.getLocation();
-        Location lastLocation = this.lastLocations.put(livingEntity, currentLocation);
+        Location currentLocation = entity.getLocation();
+        Location lastLocation = this.lastLocations.put(entity, currentLocation);
         if (lastLocation == null) lastLocation = currentLocation;
 
-        EntityId entityId = EntityId.of(livingEntity.getUniqueId(), livingEntity.getEntityId());
+        EntityId entityId = EntityId.of(entity.getUniqueId(), entity.getEntityId());
 
         if (!lastLocation.equals(currentLocation)) {
             if (lastLocation.distanceSquared(currentLocation) > 64) {
-                recordables.add(new RecEntityTeleport(entityId, currentLocation, livingEntity.isOnGround()));
+                recordables.add(new RecEntityTeleport(entityId, currentLocation, entity.isOnGround()));
             } else {
-                int amount = this.positionAndRotationAmounts.getOrDefault(livingEntity, 0);
+                int amount = this.positionAndRotationAmounts.getOrDefault(entity, 0);
                 if (amount >= 20) {
-                    recordables.add(new RecEntityCorrectPositionAndRotation(entityId, currentLocation, livingEntity.isOnGround()));
-                    this.positionAndRotationAmounts.put(livingEntity, 0);
+                    recordables.add(new RecEntityCorrectPositionAndRotation(entityId, currentLocation, entity.isOnGround()));
+                    this.positionAndRotationAmounts.put(entity, 0);
                 } else {
-                    recordables.add(new RecEntityPositionAndRotation(entityId, currentLocation, lastLocation, livingEntity.isOnGround()));
-                    this.positionAndRotationAmounts.put(livingEntity, amount + 1);
+                    recordables.add(new RecEntityPositionAndRotation(entityId, currentLocation, lastLocation, entity.isOnGround()));
+                    this.positionAndRotationAmounts.put(entity, amount + 1);
                 }
             }
         }
