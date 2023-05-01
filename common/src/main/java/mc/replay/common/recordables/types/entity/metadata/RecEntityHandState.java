018b58c808da787c4e250f81170ac510cbf9dd92
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-05-01 20:57:25 +0200

    Made new added metadata recordables entity state recordables

--- common/src/main/java/mc/replay/common/recordables/types/entity/metadata/RecEntityHandState.java
+++ common/src/main/java/mc/replay/common/recordables/types/entity/metadata/RecEntityHandState.java
@@ -1,7 +1,7 @@
 package mc.replay.common.recordables.types.entity.metadata;
 
-import mc.replay.api.recordables.Recordable;
 import mc.replay.api.recordables.data.EntityId;
+import mc.replay.common.recordables.types.internal.EntityStateRecordable;
 import mc.replay.packetlib.data.entity.PlayerHand;
 import mc.replay.packetlib.network.ReplayByteBuffer;
 import org.jetbrains.annotations.NotNull;
@@ -9,7 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import static mc.replay.packetlib.network.ReplayByteBuffer.BOOLEAN;
 
 public record RecEntityHandState(EntityId entityId, boolean handActive, PlayerHand activeHand,
-                                 boolean inSpinAttack) implements Recordable {
+                                 boolean inSpinAttack) implements EntityStateRecordable {
 
     public RecEntityHandState(@NotNull ReplayByteBuffer reader) {
         this(
--- common/src/main/java/mc/replay/common/recordables/types/entity/metadata/RecEntityHealth.java
+++ common/src/main/java/mc/replay/common/recordables/types/entity/metadata/RecEntityHealth.java
@@ -1,13 +1,13 @@
 package mc.replay.common.recordables.types.entity.metadata;
 
-import mc.replay.api.recordables.Recordable;
 import mc.replay.api.recordables.data.EntityId;
+import mc.replay.common.recordables.types.internal.EntityStateRecordable;
 import mc.replay.packetlib.network.ReplayByteBuffer;
 import org.jetbrains.annotations.NotNull;
 
 import static mc.replay.packetlib.network.ReplayByteBuffer.FLOAT;
 
-public record RecEntityHealth(EntityId entityId, float health) implements Recordable {
+public record RecEntityHealth(EntityId entityId, float health) implements EntityStateRecordable {
 
     public RecEntityHealth(@NotNull ReplayByteBuffer reader) {
         this(
